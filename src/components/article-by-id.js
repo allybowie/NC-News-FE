9414ae5b4b49cd0aa73d2e33b5eb3fdc4694e2f7
Alastair Bowie <allybowie@Alastairs-MacBook-Pro.local>
2019-11-19 09:58:37 +0000

    Show Comments In Progress

--- src/components/article-by-id.js
+++ src/components/article-by-id.js
@@ -5,20 +5,21 @@ import SingleHeader from './single-article-header';
 class ArticleById extends React.Component {
 
     state = {
-
+        commentsShown: true,
+        comments: []
     }
 
     render () {
 
 const {title, body, id, comment_count, topic, date, author, votes} = this.props
-
+const {commentsShown} = this.state
         return <div className="ArticleDiv">
-            
+
             <SingleHeader title={`${title}`} comment_count={`${comment_count}`} topic={`${topic}`} date={`${date}`} author={`${author}`} votes={`${votes}`} />
             
             <main className="ArticleBody">{body}</main>
             
-            <button className="ShowComments">Show Comments</button>
+            <button className="ShowComments">{commentsShown ? <p>Hide Comments</p> : <p>Show Comments</p>}</button>
             </div>
     }
 }
