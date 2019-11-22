3ce7f270c40858ef73945c356c89e55da55163dd
Alastair Bowie <allybowie@Alastairs-MacBook-Pro.local>
2019-11-22 11:41:45 +0000

    All core desktop CSS Done. Needs to be made responsive

--- src/components/single-article-header.js
+++ src/components/single-article-header.js
@@ -6,13 +6,23 @@ import VotesCounter from './vote';
 class SingleHeader extends React.Component {
     render() {
         const {title, comment_count, date, author, topic, votes, id, user} = this.props
+
+        console.log("TITLE LENGTH", title.length)
         
         console.log("HEADER USER", votes)
+
+        let titleClassName = "IndiHeaderTitle"
+        let authorClassName = "IndiHeaderAuthor"
+
+        if(title.length > 50){
+            titleClassName = "IndiHeaderTitle2"
+            authorClassName = "IndiHeaderAuthor2"
+        }
          
        return  <div className="IndividualHeader">
-            <p className="IndiHeaderTitle">{title}</p>
+            <p className={titleClassName}>{title}</p>
             <p className="IndiHeaderTopic">Subject: {topic}</p>
-            <p className="IndiHeaderAuthor">Written by {author}</p>
+            <p className={authorClassName}>Written by {author}</p>
             <p className="IndiHeaderDate">Posted on {date}</p>
             < VotesCounter votes={votes} id={id} className="IndiHeaderVotes" card="indiArticle" user={user}/>
         </div>
