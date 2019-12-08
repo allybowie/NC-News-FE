835067e5eb9876f87856e5e6ee544f69b6ea06cc
Alastair Bowie <allybowie@Alastairs-MacBook-Pro.local>
2019-12-08 11:35:47 +0000

    improve responsiveness. Change login from class to function

--- src/components/login.js
+++ src/components/login.js
@@ -2,30 +2,28 @@ import React from "react";
 import LoginForm from "./login-form";
 import "../App.css";
 
-class Login extends React.Component {
-  render() {
-    const { handleLogin, user } = this.props;
+const Login = props => {
+  const { handleLogin, user } = props;
 
-    return (
-      <div className="Login">
-        {user === "" ? (
-          <LoginForm handleLogin={handleLogin} />
-        ) : (
-          <label className="LogoutForm">
-            <div className="LogOutSelect"></div>
-            <button
-              className="LogoutButton"
-              onClick={() => {
-                handleLogin("");
-              }}
-            >
-              Sign Out
-            </button>
-          </label>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Login">
+      {user === "" ? (
+        <LoginForm handleLogin={handleLogin} />
+      ) : (
+        <label className="LogoutForm">
+          <div className="LogOutSelect"></div>
+          <button
+            className="LogoutButton"
+            onClick={() => {
+              handleLogin("");
+            }}
+          >
+            Sign Out
+          </button>
+        </label>
+      )}
+    </div>
+  );
+};
 
 export default Login;
